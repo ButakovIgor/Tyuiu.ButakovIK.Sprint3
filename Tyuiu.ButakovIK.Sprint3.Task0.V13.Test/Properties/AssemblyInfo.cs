ace8d90b97f4501212ae0e2f222be23386e651a9
ButakovIgor <t4w3rr@gmail.com>
2023-10-22 15:09:24 +0500

    Add project files.

---
+++ Tyuiu.ButakovIK.Sprint3.Task0.V13.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ButakovIK.Sprint3.Task0.V13.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ButakovIK.Sprint3.Task0.V13.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b1555b67-e6f3-4b71-b5a5-e1a3f3612d72")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ButakovIK.Sprint3.Task0.V13/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ButakovIK.Sprint3.Task0.V13.Lib;
+
+namespace Tyuiu.ButakovIK.Sprint3.Task0.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Бутаков И.К. | АСОиУБ-23-1";
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                  *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                   *");
+            Console.WriteLine("* Таск #0                                                                    *");
+            Console.WriteLine("* Вариант #13                                                                *");
+            Console.WriteLine("* Выполнил: Бутаков Игорь Константинович | АСОиУБ-23-1                       *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                   *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет сумму ряда по     *");
+            Console.WriteLine("* формуле, при a = 0,5                                                       *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+            Console.WriteLine("******************************************************************************");
+
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 20;
+
+            Console.WriteLine("Переменная X = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
