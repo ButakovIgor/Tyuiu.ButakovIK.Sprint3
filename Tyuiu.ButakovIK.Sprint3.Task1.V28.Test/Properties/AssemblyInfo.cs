e08d0145339cdd67e512bd20278d62e61f292e12
ButakovIgor <t4w3rr@gmail.com>
2023-10-22 16:39:14 +0500

    Task1

---
+++ Tyuiu.ButakovIK.Sprint3.Task1.V28.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ButakovIK.Sprint3.Task1.V28.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ButakovIK.Sprint3.Task1.V28.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("34e4e0df-fd22-43b2-82ec-6b4c530c5b7f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ButakovIK.Sprint3.Task1.V28/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ButakovIK.Sprint3.Task1.V28.Lib;
+
+namespace Tyuiu.ButakovIK.Sprint3.Task1.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Бутаков И.К. | АСОиУБ-23-1";
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                  *");
+            Console.WriteLine("* Тема: Оператор цикла while                                                 *");
+            Console.WriteLine("* Таск #1                                                                    *");
+            Console.WriteLine("* Вариант #28                                                                *");
+            Console.WriteLine("* Выполнил: Бутаков Игорь Константинович | АСОиУБ-23-1                       *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                   *");
+            Console.WriteLine("* Написать программу используя цикл while, которая вычисляет сумму ряда по   *");
+            Console.WriteLine("* формуле, при a = 0,5                                                       *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+            Console.WriteLine("******************************************************************************");
+
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 18;
+
+            Console.WriteLine("Переменная X = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
