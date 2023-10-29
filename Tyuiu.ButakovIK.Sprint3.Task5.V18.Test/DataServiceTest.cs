ad44511aba49d7c952841a5b7b63c294fe470c32
ButakovIgor <t4w3rr@gmail.com>
2023-10-29 21:10:59 +0500

    Task5

---
+++ Tyuiu.ButakovIK.Sprint3.Task5.V18.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ButakovIK.Sprint3.Task5.V18.Lib;
+
+namespace Tyuiu.ButakovIK.Sprint3.Task5.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService DataService = new DataService();
+
+            int x = 5;
+            int startValue1 = 1;
+            int startValue2 = 1;
+            int stopValue1 = 3;
+            int stopValue2 = 11;
+
+            double res = DataService.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+            double wait = 768.361;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ButakovIK.Sprint3.Task5.V18.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ButakovIK.Sprint3.Task5.V18.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ButakovIK.Sprint3.Task5.V18.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("99971f38-c8da-412e-8bd5-b77627e94745")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
